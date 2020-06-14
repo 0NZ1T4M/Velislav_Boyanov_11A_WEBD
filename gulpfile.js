aadcd98f730d1dffa41331f444f94df61d49ee0f
0NZ1T4M <velislav.boyanov@gmail.com>
2020-06-14 08:31:31 +0300

    Setup is done.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,20 @@ function style(){
 	return gulp.src('./scss/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('./css'))
+		//stream changes to all browsers
+		.pipe(browserSync.stream());
+}
+
+function watch(){
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	gulp.watch('./scss/**/*.scss', style);
+	gulp.watch('./*.html').on('change', browserSync.reload);
+	gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
